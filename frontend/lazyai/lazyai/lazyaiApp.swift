2e4044f47c36c693bca0f4e3d49adb6dfdfd250b
KehaoC <c13576076725@gmail.com>
2024-11-23 15:56:00 +0800

    [frontend] initialization frontend project.

---
+++ frontend/lazyai/lazyai/lazyaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyAIApp.swift
+//  LazyAI
+//
+//  Created by ALcohol_可豪 on 2024/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LazyAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
